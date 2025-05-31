b37a0c0717484ddce3fcde0cf39556eddac3b70d
ThatPalAl <alekontox@gmail.com>
2025-05-31 12:50:04 +0200

    switched aws to azure

---
+++ iac/main.tf
@@ -0,0 +1,33 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-github-iac"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "iacstorage${random_id.rand.hex}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "asp" {
+  name                = "github-iac-asp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "app" {
+  name                = "github-iac-webapp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+}
