eaa18912189e4fb6fcc00f8a65c4298f792595dc
venir <venirro@gmail.com>
2025-06-12 17:00:17 +0200

    terra fix

--- iac/variables.tf
+++ iac/variables.tf
@@ -3,3 +3,24 @@ variable "region" {
   type        = string
   default     = "West Europe"
 }
+
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure client ID (App ID)"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure client secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+}
