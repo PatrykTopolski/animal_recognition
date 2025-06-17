0378fdda844b5a2c6a48a97874f9f685ea984cc8
venir <venirro@gmail.com>
2025-06-17 16:38:18 +0200

    terraform fix docker image path

--- iac/main.tf
+++ iac/main.tf
@@ -9,7 +9,7 @@ data "azurerm_resource_group" "rg" {
   name = "rg-github-iac"
 }
 
-data "azurerm_app_service_plan" "asp" {
+data "azurerm_service_plan" "asp" {
   name                = "github-iac-asp"
   resource_group_name = data.azurerm_resource_group.rg.name
 }
@@ -18,12 +18,13 @@ resource "azurerm_linux_web_app" "app" {
   name                = "github-iac-webapp"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
-  service_plan_id     = data.azurerm_app_service_plan.asp.id
+  service_plan_id     = data.azurerm_service_plan.asp.id
 
   site_config {
     application_stack {
-      docker_image     = "githubiacregistry.azurecr.io/myapp"
-      docker_image_tag = "latest"
+      docker_image_name   = "githubiacregistry.azurecr.io/myapp"
+      docker_image_tag    = "latest"
+      docker_registry_url = "https://githubiacregistry.azurecr.io"
     }
 
     always_on = true
