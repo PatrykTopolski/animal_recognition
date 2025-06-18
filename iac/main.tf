6ec6c7e0ff9d1377b9e2bc827ecda9f6eb34fb94
venir <venirro@gmail.com>
2025-06-18 09:22:25 +0200

    terraform add app settings into resource

--- iac/main.tf
+++ iac/main.tf
@@ -9,7 +9,7 @@ data "azurerm_resource_group" "rg" {
   name = "rg-github-iac"
 }
 
-data "azurerm_service_plan" "asp" {
+data "azurerm_app_service_plan" "asp" {
   name                = "github-iac-asp"
   resource_group_name = data.azurerm_resource_group.rg.name
 }
@@ -18,20 +18,18 @@ resource "azurerm_linux_web_app" "app" {
   name                = "github-iac-webapp"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
-  service_plan_id     = data.azurerm_service_plan.asp.id
+  service_plan_id     = data.azurerm_app_service_plan.asp.id
 
   site_config {
-    app_settings = {
-      WEBSITES_PORT = "8080"
-    }
-    always_on = true
+    linux_fx_version = "DOCKER|githubiacregistry.azurecr.io/myapp:latest"
+    always_on        = true
   }
 
-  container_settings {
-    image_name          = "githubiacregistry.azurecr.io/myapp:latest"
-    registry_server_url = "https://githubiacregistry.azurecr.io"
-    registry_username   = var.docker_username
-    registry_password   = var.docker_password
+  app_settings = {
+    WEBSITES_PORT                    = "8080"
+    DOCKER_REGISTRY_SERVER_URL      = "https://githubiacregistry.azurecr.io"
+    DOCKER_REGISTRY_SERVER_USERNAME = var.docker_username
+    DOCKER_REGISTRY_SERVER_PASSWORD = var.docker_password
   }
 }
 
