65f017ceadcbe7b39e115a57936ebb1038d0028b
venir <venirro@gmail.com>
2025-06-18 09:38:17 +0200

    terraform add application settings delete container stack

--- iac/main.tf
+++ iac/main.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.81.0"
+      version = "~> 4.33.0"
     }
   }
-
-  required_version = ">= 1.5.0"
 }
 
 provider "azurerm" {
@@ -32,21 +30,20 @@ resource "azurerm_linux_web_app" "app" {
   service_plan_id     = data.azurerm_service_plan.asp.id
 
   site_config {
-    always_on = true
-
-    container_settings {
-      image_name          = "githubiacregistry.azurecr.io/myapp:latest"
-      registry_server_url = "https://githubiacregistry.azurecr.io"
-      registry_username   = var.docker_username
-      registry_password   = var.docker_password
+    application_stack {
+      docker_image     = "githubiacregistry.azurecr.io/myapp"
+      docker_image_tag = "latest"
     }
+    always_on = true
   }
 
   app_settings = {
-    WEBSITES_PORT = "8080"
+    WEBSITES_PORT                    = "8080"
+    DOCKER_REGISTRY_SERVER_URL      = "https://githubiacregistry.azurecr.io"
+    DOCKER_REGISTRY_SERVER_USERNAME = var.docker_username
+    DOCKER_REGISTRY_SERVER_PASSWORD = var.docker_password
   }
 }
-
 
 output "app_url" {
   value = azurerm_linux_web_app.app.default_hostname
