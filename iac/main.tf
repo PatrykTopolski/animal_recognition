8acd2d67f774b90c1632228bd36e52ad6add6508
venir <venirro@gmail.com>
2025-06-18 10:36:00 +0200

    terraform changed docker to acr

--- iac/main.tf
+++ iac/main.tf
@@ -13,9 +13,6 @@ provider "azurerm" {
   features {}
 }
 
-variable "docker_username" {}
-variable "docker_password" {}
-
 data "azurerm_resource_group" "rg" {
   name = "rg-github-iac"
 }
@@ -25,27 +22,38 @@ data "azurerm_service_plan" "asp" {
   resource_group_name = data.azurerm_resource_group.rg.name
 }
 
+data "azurerm_container_registry" "acr" {
+  name                = "githubiacregistry"
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
 resource "azurerm_linux_web_app" "app" {
   name                = "github-iac-webapp"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   service_plan_id     = data.azurerm_service_plan.asp.id
 
+  identity {
+    type = "SystemAssigned"
+  }
+
   site_config {
     always_on = true
   }
 
   app_settings = {
-    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
-    WEBSITES_PORT                       = "8080"
-    DOCKER_REGISTRY_SERVER_URL          = "https://githubiacregistry.azurecr.io"
-    DOCKER_REGISTRY_SERVER_USERNAME     = var.docker_username
-    DOCKER_REGISTRY_SERVER_PASSWORD     = var.docker_password
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE  = "false"
+    WEBSITES_PORT                        = "8080"
     WEBSITES_CONTAINER_START_TIME_LIMIT = "1800"
     WEBSITES_CONTAINER_IMAGE             = "githubiacregistry.azurecr.io/myapp:latest"
   }
 }
 
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id         = azurerm_linux_web_app.app.identity.principal_id
+  role_definition_name = "AcrPull"
+  scope                = data.azurerm_container_registry.acr.id
+}
 
 output "app_url" {
   value = azurerm_linux_web_app.app.default_hostname
