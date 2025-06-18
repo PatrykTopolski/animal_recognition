e569b89a5a7642d9eeb4a19f1c4a3f0580fc3b5c
venir <venirro@gmail.com>
2025-06-18 11:29:56 +0200

    fix unhash azurerm_role_assignment

--- iac/main.tf
+++ iac/main.tf
@@ -51,13 +51,13 @@ resource "azurerm_linux_web_app" "app" {
 
 
 }
-#
-# resource "azurerm_role_assignment" "acr_pull" {
-#   scope                = data.azurerm_container_registry.acr.id
-#   role_definition_name = "AcrPull"
-#   principal_id         = azurerm_linux_web_app.app.identity[0].principal_id
-#   depends_on = [azurerm_linux_web_app.app]
-# }
+
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = data.azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_linux_web_app.app.identity[0].principal_id
+  depends_on = [azurerm_linux_web_app.app]
+}
 
 
 output "app_url" {
